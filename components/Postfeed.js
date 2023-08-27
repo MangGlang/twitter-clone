c22f2a6418940f5cb0d8d194ffa2f3628663b9ba
MangGlang <71730799+MangGlang@users.noreply.github.com>
2023-08-27 14:41:54 -0700

    Trending Component

--- components/Postfeed.js
+++ components/Postfeed.js
@@ -4,7 +4,7 @@ import TweetInput from "./TweetInput";
 export default function Postfeed() {
   return (
     <div
-      className="sm:ml-20 xl:ml-96 max-w-2xl flex-grow
+      className="sm:ml-16 xl:ml-[350px] max-w-2xl flex-grow
         border-gray-700 border-x"
     >
       <div className="px-3 py-2 text-lg sm:text-xl font-bold border-b border-gray-700
